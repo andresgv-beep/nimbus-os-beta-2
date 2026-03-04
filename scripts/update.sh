c255a0bea15f5fb9813787fab62d76e67132bced
andresgv-beep <andresgv7455@gmail.com>
2026-03-04 19:28:06 +0100

    Add files via upload

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 DIR="/opt/nimbusos"
-URL="https://github.com/andresgv-beep/nimbus-os-beta-1/archive/refs/heads/main.tar.gz"
+URL="https://github.com/andresgv-beep/nimbus-os-beta-2/archive/refs/heads/main.tar.gz"
 RESULT_FILE="/var/log/nimbusos/update-result.json"
 
 PREV=$(node -e "console.log(require('$DIR/package.json').version)" 2>/dev/null)
 echo "Current: $PREV"
 
 # Save checksums of key files to detect changes
-SERVER_HASH=$(md5sum "$DIR/server/index.cjs" 2>/dev/null | cut -d' ' -f1)
+SERVER_HASH=$(find "$DIR/server" -name "*.cjs" -exec md5sum {} \; 2>/dev/null | sort | md5sum | cut -d' ' -f1)
 INSTALL_HASH=$(md5sum "$DIR/install.sh" 2>/dev/null | cut -d' ' -f1)
 
 echo "Downloading..."
@@ -24,7 +24,7 @@ rm -rf dist
 npx vite build 2>&1 | tail -1
 
 # Determine restart type based on what changed
-SERVER_HASH_NEW=$(md5sum "$DIR/server/index.cjs" 2>/dev/null | cut -d' ' -f1)
+SERVER_HASH_NEW=$(find "$DIR/server" -name "*.cjs" -exec md5sum {} \; 2>/dev/null | sort | md5sum | cut -d' ' -f1)
 INSTALL_HASH_NEW=$(md5sum "$DIR/install.sh" 2>/dev/null | cut -d' ' -f1)
 
 if [ "$SERVER_HASH" != "$SERVER_HASH_NEW" ] || [ "$INSTALL_HASH" != "$INSTALL_HASH_NEW" ]; then
